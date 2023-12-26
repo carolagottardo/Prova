aaaf5b49a785761161cbc7e574c9bc217530130e
Carola Gottardo <46298786+carolagottardo@users.noreply.github.com>
2023-12-26 11:48:16 +0100

    Delete MatriceException4.java

--- MatriceException4.java
+++
@@ -1,9 +0,0 @@
-public class MatriceException4 extends RuntimeException {
-    public MatriceException4() {
-        super();
-    }
-
-    public MatriceException4(String message) {
-        super(message);
-    }
-}
